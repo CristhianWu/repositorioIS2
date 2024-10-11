9f8135b2c3f83c2c3810e241d1d7bb3f95c19656
Ian Cedeño <ianfarith03@gmail.com>
2024-10-11 12:13:15 -0500

    cambio de titulo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Grupo de Ian, Carlos y Cristhian </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
